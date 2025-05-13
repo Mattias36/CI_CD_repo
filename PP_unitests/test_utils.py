8840cc13a6c02947d88b9e5a8db5b19dd9a39fd7
Mati <mateuszmarciak36@gmail.com>
2025-05-13 14:16:52 +0200

    changed the parameters for test_add

--- PP_unitests/test_utils.py
+++ PP_unitests/test_utils.py
@@ -2,7 +2,7 @@ import pytest
 import utils
 
 
-@pytest.mark.parametrize("a, b, expected ", [(1, 2, 3), (2, 3,
+@pytest.mark.parametrize("a, b, expected ", [(10, 11, 21), (2, 3,
                                                          5), (3, 4, 7), (4, 5, 9)])
 def test_add(a, b, expected):
     result = utils . add(a, b)
